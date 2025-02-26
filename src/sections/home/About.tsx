01e5ad66cd35e530612240591ab6d4a7625a1d96
ranimRefaie <ranemalre@gmail.com>
2025-02-26 16:27:45 +0200

    Initial commit

--- src/sections/home/About.tsx
+++ src/sections/home/About.tsx
@@ -10,7 +10,7 @@ export const About = () => {
 
       <div className="flex-col w-[89%] flex md:flex-row mx-auto justify-center gap-11">
         <Image src={img_about} alt="" width={624} height={476} />
-        <div className="about-description w-[48%]">
+        <div className="about-description w-full md:w-[48%]">
           <h1 className="text-[20px] leading-[20.11px] md:text-[48px] md:leading-[72px] font-normal text-[#293352]"> EUROQUEST</h1>
           <p className="text-xs leading-[15.08px] md:text-[14px] font-normal md:leading-[30px] text-[#080808] my-6 capitalize ">
           Balanced Score Training Center is an international training institute that seeks to enhance organizational performance and professional skills through its structured programs across different organizational levels. Our aim is to help organizations and people with creative training and consulting solutions that combine theoretical knowledge with practical application, with a view to achieving effective and lasting outcomes          </p>
